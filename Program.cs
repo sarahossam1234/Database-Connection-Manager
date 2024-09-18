2a4deef23604ea6b58e37fc4df8ec10a8d0849bd
sarahossam1234 <158767344+sarahossam1234@users.noreply.github.com>
2024-09-18 14:46:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Database db = new Database();
+
+        DatabaseConnectionParameters dbParams = new DatabaseConnectionParameters(
+            "myServer", "myDatabase", "myUser", "myPassword"
+        );
+
+        string filePath = "connection_params.STD";
+        db.SaveConnectionString(filePath, dbParams);
+
+        db.LoadConnectionString(filePath);
+        Console.WriteLine("Connection String: " + db.GetConnectionString());
+    }
+}
